2dc95356cf402046408278a899bead67abe584ec
Filip Borowy <fborowy03@gmail.com>
2024-04-10 13:19:28 +0200

    gradle dependencies and UI details update

--- app/src/main/java/pl/fboro/taski/feature_task/presentation/components/TasksContent.kt
+++ app/src/main/java/pl/fboro/taski/feature_task/presentation/components/TasksContent.kt
@@ -1,14 +1,15 @@
 package pl.fboro.taski.feature_task.presentation.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import pl.fboro.taski.feature_task.data.PresentationMode
@@ -21,13 +22,14 @@ fun TasksContent(
     navController: NavController,
     onEvent: (TaskEvent) -> Unit
 ) {
+    val context = LocalContext.current
     var category by remember{ mutableStateOf(0) }
 
     Column(
         modifier = Modifier
             .fillMaxWidth()
     ) {
-        Text(
+        Row(
             modifier = Modifier
                 .padding(20.dp)
                 .clickable {
@@ -46,13 +48,22 @@ fun TasksContent(
                             else -> PresentationMode.SHOW_ALL_SPECIFIED_DATE
                         }))
                     }
-                },
-            text = when (category){
-            0 -> "Nadchodzące"
-            1 -> "Zrobione"
-            2 -> "Wszystkie"
-            else -> "Błąd"
-        })
+                }
+        ) {
+            Text(
+                text = when (category){
+                    0 -> "Nadchodzące"
+                    1 -> "Zrobione"
+                    2 -> "Wszystkie"
+                    else -> "Błąd"
+            })
+            Spacer(modifier = Modifier.width(10.dp))
+            Image(
+                painter = painterResource(id = pl.fboro.taski.R.drawable.ic_cached_30),
+                contentDescription = context.resources.getString(pl.fboro.taski.R.string.change_task_showing_option_image)
+            )
+        }
+
         Column(modifier = Modifier
             .padding(horizontal = 15.dp)
             .verticalScroll(rememberScrollState())
